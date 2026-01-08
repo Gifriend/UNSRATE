5b979f9bdf7e79bd2f5b9b3fbd0f87464f70e1dd
MikelNoppo <mikelnoppo03@gmail.com>
2026-01-08 09:14:02 +0800

    feat: integrate Svelte Auth client for improved authentication handling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { createSvelteAuthClient } from '@mmailaender/convex-better-auth-svelte/svelte'; 
+  	import { authClient } from '$lib/auth-client';
+	
+	createSvelteAuthClient({ authClient });
+	
 	let { children } = $props();
 </script>
 
